dd3a3f26ceb392db6cadd2337f5c1782d578da79
Sarah Tumenas <sarah.marques@spread.com.br>
2022-07-25 10:28:25 -0300

    Domain Model - Entidades vs Value Objects

--- src/main/java/br/com/alura/escola/CPF.java
+++ src/main/java/br/com/alura/escola/CPF.java
@@ -5,7 +5,7 @@ public class CPF {
     private String numero;
 
     public CPF(String numero) {
-        if (numero == null || !numero.matches("/^(([0-9]{3}.[0-9]{3}.[0-9]{3}-[0-9]{2})|([0-9]{11}))$/")){
+        if (numero == null || !numero.matches("^(([0-9]{3}.[0-9]{3}.[0-9]{3}-[0-9]{2})|([0-9]{11}))$")){
             throw new IllegalArgumentException("CPF invalido");
         }
         this.numero = numero;
