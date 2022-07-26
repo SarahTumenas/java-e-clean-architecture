de074db1989c2666001f2a9e4314d87284553ea9
Sarah Tumenas <sarah.marques@spread.com.br>
2022-07-26 14:33:30 -0300

    Interface com usuário

---
+++ src/main/java/br/com/alura/escola/MatricularAluno.java
@@ -0,0 +1,22 @@
+package br.com.alura.escola;
+
+
+import br.com.alura.escola.dominio.aluno.Aluno;
+import br.com.alura.escola.dominio.aluno.CPF;
+import br.com.alura.escola.dominio.aluno.Email;
+import br.com.alura.escola.dominio.aluno.RepositorioDeAlunos;
+import br.com.alura.escola.infra.aluno.RepositorioDeAlunosEmMemoria;
+
+public class MatricularAluno {
+
+    public static void main(String[] args) {
+        String nome = "Fulano da Silva";
+        CPF cpf = new CPF("123.456.789-00");
+        Email email = new Email( "fulano@email.com");
+
+        Aluno aluno = new Aluno(nome, cpf, email);
+        RepositorioDeAlunos repositorioDeAlunos = new RepositorioDeAlunosEmMemoria();
+        repositorioDeAlunos.matricular(aluno);
+
+    }
+}
